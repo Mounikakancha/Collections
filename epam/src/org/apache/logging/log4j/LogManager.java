1ee00f4addc494f42db046f32f45ae8c1d24c309
91798 <91798@DESKTOP-JF52KRC>
2020-03-09 21:22:31 +0530

    first commit

---
+++ epam/src/org/apache/logging/log4j/LogManager.java
@@ -0,0 +1,12 @@
+package org.apache.logging.log4j;
+
+import epam.App;
+
+public class LogManager {
+
+	public static Logger getLogger(Class<App> class1) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
